d53905b014804ce1de0d6f38f9078ddb20dd3760
litedogedev <litedogedev>
2015-03-19 01:24:59 +0000

    nodes

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -84,9 +84,9 @@ public:
         assert(hashGenesisBlock == uint256("0x0000032101032f27e7cdddb1196353f7fc9e1b6294717432135add95534f67c6"));
         assert(genesis.hashMerkleRoot == uint256("0x6e8089863e3811437cca6029c8eb113e0ddec4cb553e7cfdf8944c964cf86832"));
 
-        vSeeds.push_back(CDNSSeedData("", ""));
-        vSeeds.push_back(CDNSSeedData("", ""));
-        vSeeds.push_back(CDNSSeedData("", ""));
+        vSeeds.push_back(CDNSSeedData("198.105.125.233", "198.105.125.233"));
+        vSeeds.push_back(CDNSSeedData("198.105.125.234", "198.105.125.234"));
+        vSeeds.push_back(CDNSSeedData("198.105.122.174", "198.105.122.174"));
 
         base58Prefixes[PUBKEY_ADDRESS] = list_of(90);
         base58Prefixes[SCRIPT_ADDRESS] = list_of(8);
