ab02e967a887c4fb3bd0b87e6cc2f651026a4847
suchapp <wow@suchapp.me>
2017-12-31 02:38:13 +0200

    Fixed 'ambiguous overload error'

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -96,11 +96,11 @@ public:
         vSeeds.push_back(CDNSSeedData("93.157.4.11", "93.157.4.11"));
         vSeeds.push_back(CDNSSeedData("37.187.99.136", "37.187.99.136"));
 
-        base58Prefixes[PUBKEY_ADDRESS] = list_of(90);
-        base58Prefixes[SCRIPT_ADDRESS] = list_of(8);
-        base58Prefixes[SECRET_KEY] =     list_of(171);
-        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x88)(0xB2)(0x1E);
-        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x88)(0xAD)(0xE4);
+	base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,90);
+	base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,8);
+	base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1,171);
+	base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
+	base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
     }
@@ -148,11 +148,11 @@ public:
         vFixedSeeds.clear();
         vSeeds.clear();
 
-        base58Prefixes[PUBKEY_ADDRESS] = list_of(111);
-        base58Prefixes[SCRIPT_ADDRESS] = list_of(196);
-        base58Prefixes[SECRET_KEY]     = list_of(239);
-        base58Prefixes[EXT_PUBLIC_KEY] = list_of(0x04)(0x35)(0x87)(0xCF);
-        base58Prefixes[EXT_SECRET_KEY] = list_of(0x04)(0x35)(0x83)(0x94);
+	base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,111);
+	base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,196);
+	base58Prefixes[SECRET_KEY]     = std::vector<unsigned char>(1,239);
+	base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x35)(0x87)(0xCF).convert_to_container<std::vector<unsigned char> >();
+	base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x35)(0x83)(0x94).convert_to_container<std::vector<unsigned char> >();
 
         convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
 
