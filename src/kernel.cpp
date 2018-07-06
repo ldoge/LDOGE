8c9845290e3453cfc77d44b156633a689aa9067a
vashshawn <shawnvash@gmail.com>
2018-07-06 16:12:27 -0700

    Update kernel.cpp

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -1,5 +1,6 @@
-// Copyright (c) 2012-2013 The PPCoin developers
-// Copyright (c) 2014 The BlackCoin developers
+// Copyright (c) 2012-2013 The PPC developers
+// Copyright (c) 2014 The BLK developers
+// Copyright (c) 2018 The Litedoge developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
