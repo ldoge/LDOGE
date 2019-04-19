3deaed33f814fb40e1a52d84a3ac5ddad888e16e
Shawn M <shawnvash@gmail.com>
2019-04-19 02:18:15 -0700

    Create autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+set -e
+srcdir="$(dirname $0)"
+cd "$srcdir"
+if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
+  LIBTOOLIZE="${GLIBTOOLIZE}"
+  export LIBTOOLIZE
+fi
+autoreconf --install --force --warnings=all
+© 2019 GitHub, Inc.
