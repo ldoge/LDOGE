2cc6584ca773a2efa23746bb2372ab70acddd4e0
Shawn M <shawnvash@gmail.com>
2019-04-24 21:52:37 -0700

    Update guiconstants.h

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -14,11 +14,15 @@ static const int STATUSBAR_ICONSIZE = 16;
 #define STYLE_INVALID "background:#FF8080"
 
 /* Transaction list -- unconfirmed transaction */
-#define COLOR_UNCONFIRMED QColor(128, 128, 128)
+#define COLOR_UNCONFIRMED QColor(140, 140, 140)
 /* Transaction list -- negative amount */
-#define COLOR_NEGATIVE QColor(255, 0, 0)
+#define COLOR_NEGATIVE QColor(255, 58, 66)
+/* Table List -- negative amount */
+#define COLOR_NEGATIVE_TABLE QColor(255, 58, 66)
+/* Transaction list -- positive amount */
+#define COLOR_POSITIVE QColor(60, 176, 84)
 /* Transaction list -- bare address (without label) */
-#define COLOR_BAREADDRESS QColor(140, 140, 140)
+#define COLOR_BAREADDRESS QColor(171, 171, 171)
 
 /* Tooltips longer than this (in characters) are converted into rich text,
    so that they can be word-wrapped.
@@ -30,5 +34,10 @@ static const int MAX_URI_LENGTH = 255;
 
 /* QRCodeDialog -- size of exported QR Code image */
 #define EXPORT_IMAGE_SIZE 256
+
+/* Colors for minting tab for each coin age group */
+#define COLOR_MINT_YOUNG QColor(255, 224, 226)
+#define COLOR_MINT_MATURE QColor(204, 255, 207)
+#define COLOR_MINT_OLD QColor(51, 64, 5
 
 #endif // GUICONSTANTS_H
