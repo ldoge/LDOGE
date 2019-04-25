0a325094a7f296755f66e16e779fbd5a104bdfee
Shawn M <shawnvash@gmail.com>
2019-04-24 22:24:51 -0700

    Update extract_strings_qt.py

--- share/qt/extract_strings_qt.py
+++ share/qt/extract_strings_qt.py
@@ -5,6 +5,7 @@ they can be picked up by Qt linguist.
 '''
 from subprocess import Popen, PIPE
 import glob
+import operator
 
 OUT_CPP="src/qt/bitcoinstrings.cpp"
 EMPTY=['""']
@@ -51,7 +52,7 @@ files = glob.glob('src/*.cpp') + glob.glob('src/*.h')
 child = Popen(['xgettext','--output=-','-n','--keyword=_'] + files, stdout=PIPE)
 (out, err) = child.communicate()
 
-messages = parse_po(out) 
+messages = parse_po(str(out))
 
 f = open(OUT_CPP, 'w')
 f.write("""#include <QtGlobal>
