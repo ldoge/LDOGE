c39c42b84d51e7627233b652f24db8ff272b0fa5
Shawn M <shawnvash@gmail.com>
2019-05-01 11:44:56 -0700

    Update transactionrecord.h

--- src/qt/transactionrecord.h
+++ src/qt/transactionrecord.h
@@ -1,6 +1,11 @@
+// Copyright (c) 2011-2013 The Bitcoin Core developers
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef TRANSACTIONRECORD_H
 #define TRANSACTIONRECORD_H
 
+#include "amount.h"
 #include "uint256.h"
 
 #include <QList>
