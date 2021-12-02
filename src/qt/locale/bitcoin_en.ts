f372ac7aac024e514faa14e3f467dab9c24838cb
Shawn M <shawnvash@gmail.com>
2021-12-02 14:15:55 -0800

    Update bitcoin_en.ts

--- src/qt/locale/bitcoin_en.ts
+++ src/qt/locale/bitcoin_en.ts
@@ -15,13 +15,12 @@
     </message>
     <message>
         <location line="+41"/>
-        <source>Copyright © 2009-2015 The Bitcoin developers
-Copyright © 2011-2021 PPCoin Developers
-Copyright © 2012-2020 The LiteDogecoin developers
-Copyright © 2014-2015 The BlackCoin developers
-Copyright © 2015 The LiteDoge developers</source>
-        <translation type="unfinished"></translation>
-    </message>
+        <source>Copyright © 2009-2022 The Bitcoin developers
+Copyright © 2011-2022 PPCoin Developers
+Copyright © 2014-2022 The BlackCoin developers
+Copyright © 2015-2022 The LiteDogecoin developers</source>
+    <translation type="unfinished"></translation>
+     </message>
     <message>
         <location line="+15"/>
         <source>
