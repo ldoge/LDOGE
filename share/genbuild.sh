8486cc060ce6dc210fa7b38517c3e95de5f885d6
Shawn M <shawnvash@gmail.com>
2021-12-31 19:05:53 -0800

    Update genbuild.sh

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -8,6 +8,21 @@ fi
 SCRIPT=$(readlink -f "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 
+# Dependencies
+apt-get update || exit
+apt-get install -y gcc g++ autoconf libtool libboost-all-dev pkg-config make libminiupnpc-dev || exit
+# Can't find libdb 4.8 in debian repos
+dpkg -i $SCRIPTPATH/libdb4.8_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i $SCRIPTPATH/libdb4.8-dev_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i $SCRIPTPATH/libdb4.8++_4.8.24-1ubuntu1_i386.deb || exit
+dpkg -i $SCRIPTPATH/libdb4.8++-dev_4.8.24-1ubuntu1_i386.deb || exit
+
+git clone https://github.com/ldoge/LDOGE.git
+cd litedoge
+./autogen.sh
+./configure
+make
+
 if [ $# -gt 0 ]; then
     FILE="$1"
     shift
@@ -41,18 +56,3 @@ if [ "$INFO" != "$NEWINFO" ]; then
     echo "$NEWINFO" >"$FILE"
     echo "#define BUILD_DATE \"$TIME\"" >>"$FILE"
 fi
-
-# Dependencies
-apt-get update || exit
-apt-get install -y gcc g++ autoconf libtool libboost-all-dev pkg-config make libminiupnpc-dev || exit
-# Can't find libdb 4.8 in debian repos
-dpkg -i $SCRIPTPATH/libdb4.8_4.8.24-1ubuntu1_i386.deb || exit
-dpkg -i $SCRIPTPATH/libdb4.8-dev_4.8.24-1ubuntu1_i386.deb || exit
-dpkg -i $SCRIPTPATH/libdb4.8++_4.8.24-1ubuntu1_i386.deb || exit
-dpkg -i $SCRIPTPATH/libdb4.8++-dev_4.8.24-1ubuntu1_i386.deb || exit
-
-git clone https://github.com/ldoge/LDOGE.git
-cd litedoge
-./autogen.sh
-./configure
-make
