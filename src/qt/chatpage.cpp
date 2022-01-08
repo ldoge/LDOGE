bc8fd102127a45f2f083aa00da99e5dbc3adb5c3
Shawn M <shawnvash@gmail.com>
2022-01-08 04:26:04 -0800

    Update chatpage.cpp

--- src/qt/chatpage.cpp
+++ src/qt/chatpage.cpp
@@ -1,5 +1,5 @@
 #include "chatpage.h"
-#include "ircchat.h"
+#include "ui_chatpage.h"
 
 //#include "clientmodel.h"
 #include "walletmodel.h"
@@ -8,7 +8,16 @@
 #include "cookiejar.h"
 #include "webview.h"
 
-using namespace GUIUtil;
+using namespace GUIUtiL;
+
+// Setup header and styles
+//    if (fNoHeaders)
+//        GUIUtil::header(this, QString(""));
+//    else if (fSmallHeaders)
+//        GUIUtil::header(this, QString(":images/headerChatSmall"));
+//    else
+//        GUIUtil::header(this, QString(":images/headerChat"));
+//    this->layout()->setContentsMargins(0, HEADER_HEIGHT, 0, 0); 
 
 ChatPage::ChatPage(QWidget *parent) :
     QWidget(parent),
@@ -16,36 +25,16 @@ ChatPage::ChatPage(QWidget *parent) :
     walletModel(0)
 {
     ui->setupUi(this);
-
-    // Setup header and styles
-//    if (fNoHeaders)
-//        GUIUtil::header(this, QString(""));
-//    else if (fSmallHeaders)
-//        GUIUtil::header(this, QString(":images/headerChatSmall"));
-//    else
-//        GUIUtil::header(this, QString(":images/headerChat"));
-//    this->layout()->setContentsMargins(0, HEADER_HEIGHT, 0, 0);     
-        
+    
     CookieJar *chatJar = new CookieJar;
     ui->webView->page()->networkAccessManager()->setCookieJar(chatJar);
 
     ui->webView->page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAsNeeded);
-    ui->webView->page()->setLinkDelegationPolicy(QWebPage::DelegateAllLinks);
-    ui->frame->setVisible(true); // Set to true to enable webView navigation buttons    
+    ui->webView->page()->setLinkDelegationPolicy(QWebPage::DelegateAllLinks);  
     connect(ui->webView->page()->networkAccessManager(), SIGNAL(sslErrors(QNetworkReply*, const QList<QSslError> & )), ui->webView, SLOT(sslErrorHandler(QNetworkReply*, const QList<QSslError> & )));
     connect(ui->webView->page(), SIGNAL(linkClicked(QUrl)), ui->webView, SLOT(myOpenUrl(QUrl)));
 
-    // buttons
-    ui->back->setDisabled(true);
-    ui->home->setDisabled(true);
-    ui->forward->setDisabled(true);
-    ui->webView->sendButtons(ui->back, ui->home, ui->forward);
-    connect(ui->back, SIGNAL(clicked()), ui->webView, SLOT(myBack()));
-    connect(ui->home, SIGNAL(clicked()), ui->webView, SLOT(myHome()));
-    connect(ui->forward, SIGNAL(clicked()), ui->webView, SLOT(myForward()));
-    connect(ui->reload, SIGNAL(clicked()), ui->webView, SLOT(myReload()));
-            
-}
+}   
 
 ChatPage::~ChatPage()
 {
@@ -57,6 +46,4 @@ void ChatPage::setModel(WalletModel *model)
     this->walletModel = model;
 
     ui->webView->load(QUrl("https://web.libera.chat/#litedoge"));
-    //    QUrl url(QString(walletUrl).append("wallet/chat.php?v=").append(FormatVersion(CLIENT_VERSION).c_str()));
-    //    ui->webView->myOpenUrl(url);
 }
