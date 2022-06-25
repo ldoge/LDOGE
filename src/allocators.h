e1adcac3398dc193fd5204ccd06e856a77574582
Shawn M <shawnvash@gmail.com>
2022-06-25 05:14:13 -0700

    Update allocators.h

--- src/allocators.h
+++ src/allocators.h
@@ -1,5 +1,6 @@
 // Copyright (c) 2009-2022 Satoshi Nakamoto
 // Copyright (c) 2009-2022 The Bitcoin developers
+// Copyright (c) 2015 - 2022 The Litedoge developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #ifndef BITCOIN_ALLOCATORS_H
