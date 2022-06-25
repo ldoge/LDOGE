d0f703931d9e9690107fb82d52f18fa15eceba5b
Shawn M <shawnvash@gmail.com>
2022-06-25 04:50:47 -0700

    Update version.cpp

--- src/version.cpp
+++ src/version.cpp
@@ -1,4 +1,5 @@
-// Copyright (c) 2012-2021 The Bitcoin developers
+// Copyright (c) 2012-2022 The Bitcoin developers
+// Copyright (c) 2012-2022 The Litedoge developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <string>
