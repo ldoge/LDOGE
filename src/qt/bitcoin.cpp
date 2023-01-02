a6b9d14d396c17fc68011c81122d42b419d59911
Shawn M <shawnvash@gmail.com>
2023-01-02 02:19:15 -0800

    Update bitcoin.cpp

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -139,7 +139,8 @@ int main(int argc, char *argv[])
     #if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
 	QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 	QApplication::setStyle(QStyleFactory::create("Fusion"));
-    
+    #endif
+	
     QApplication app(argc, argv);
 
     // Do this early as we don't want to bother initializing if we are just calling IPC
