2c28d692dc23354a113c4fab4e1b8441ec3508de
Shawn M <shawnvash@gmail.com>
2023-02-04 03:20:21 -0800

    Update serveur.cpp

--- src/qt/serveur.cpp
+++ src/qt/serveur.cpp
@@ -14,7 +14,7 @@
 * License along with this program; if not, write to the Free Software
 * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA*/
 #include <QScrollBar>
-#include "server.h"
+#include "serveur.h"
         QStringList users;
         bool delist = true;
 server::server()
