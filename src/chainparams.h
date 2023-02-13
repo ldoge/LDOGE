9c9ca93336e914dac1be94e9a8f9b49fac44155e
Shawn M <shawnvash@gmail.com>
2023-02-13 03:02:56 -0800

    Update chainparams.h

--- src/chainparams.h
+++ src/chainparams.h
@@ -1,11 +1,15 @@
-// Copyright (c) 2009-2022 Satoshi Nakamoto
-// Copyright (c) 2009-2013 The Bitcoin developers
+// Copyright (c) 2009-2023 Satoshi Nakamoto
+// Copyright (c) 2009-2023 The Bitcoin developers
+// Copyright (c) 2009-2023 The Litedoge developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_CHAIN_PARAMS_H
 #define BITCOIN_CHAIN_PARAMS_H
-
+#include "chainparamsbase.h"
+#include "consensus/params.h"
+#include "primitives/block.h"
+#include "protocol.h"
 #include "bignum.h"
 #include "uint256.h"
 #include "util.h"
@@ -52,11 +56,28 @@ public:
 
         MAX_BASE58_TYPES
     };
-
+    
+    const Consensus::Params& GetConsensus(uint32_t nTargetHeight) const {
+        return *(pConsensusRoot -> GetConsensus(nTargetHeight));
+    }
+    
+    const CMessageHeader::MessageStartChars& MessageStart() const { return pchMessageStart; }
+     int GetDefaultPort() const { return nDefaultPort; }
+    
+    const CBlock& GenesisBlock() const { return genesis; }
+    /** Make miner wait to have peers to avoid wasting work */
+     const CBlock& GenesisBlock() const { return genesis; }
+    /** Make miner wait to have peers to avoid wasting work */
+    bool MiningRequiresPeers() const { return fMiningRequiresPeers; }
+    /** Default value for -checkmempool and -checkblockindex argument */
+    bool DefaultConsistencyChecks() const { return fDefaultConsistencyChecks; }
+    /** Policy: Filter transactions that do not match well-defined patterns */
+    bool RequireStandard() const { return fRequireStandard; }
+    uint64_t PruneAfterHeight() const { return nPruneAfterHeight; }
+    /** Make miner stop after a block is found. In RPC, don't return until nGenProcLimit blocks are generated */
+    bool MineBlocksOnDemand() const { return fMineBlocksOnDemand; }
+    /** Return the BIP70 network string (main) */
     const uint256& HashGenesisBlock() const { return hashGenesisBlock; }
-    const MessageStartChars& MessageStart() const { return pchMessageStart; }
-    const vector<unsigned char>& AlertKey() const { return vAlertPubKey; }
-    int GetDefaultPort() const { return nDefaultPort; }
     const CBigNum& ProofOfWorkLimit() const { return bnProofOfWorkLimit; }
     int SubsidyHalvingInterval() const { return nSubsidyHalvingInterval; }
     virtual const CBlock& GenesisBlock() const = 0;
@@ -72,8 +93,6 @@ protected:
 
     uint256 hashGenesisBlock;
     MessageStartChars pchMessageStart;
-    // Raw pub key bytes for the broadcast alert signing key.
-    vector<unsigned char> vAlertPubKey;
     int nDefaultPort;
     int nRPCPort;
     CBigNum bnProofOfWorkLimit;
@@ -81,6 +100,24 @@ protected:
     string strDataDir;
     vector<CDNSSeedData> vSeeds;
     std::vector<unsigned char> base58Prefixes[MAX_BASE58_TYPES];
+    uint64_t nPruneAfterHeight;
+    std::vector<CDNSSeedData> vSeeds;
+    std::vector<unsigned char> base58Prefixes[MAX_BASE58_TYPES];
+    std::string strNetworkID;
+    CBlock genesis;
+    std::vector<SeedSpec6> vFixedSeeds;
+    bool fMiningRequiresPeers;
+    bool fDefaultConsistencyChecks;
+    bool fRequireStandard;
+    bool fMineBlocksOnDemand;
+    CCheckpointData checkpointData;
+    ChainTxData chainTxData;
+    
+    int nMaxReorganizationDepth;
+    int nMinReorganizationPeers;
+
+    int nSwitchKimotoGravity;
+
 };
 
 /**
