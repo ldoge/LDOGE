992aecb30ab04119dcb88b21823d9307edfad5b1
NoNameDude <62181945+NoNameDude@users.noreply.github.com>
2023-02-13 14:23:02 +0100

    Request Check
    I made a request check so that the users have to type y in order to remove the password of the wallet

--- contrib/wallettools/walletchangepass.py
+++ contrib/wallettools/walletchangepass.py
@@ -2,4 +2,18 @@ from jsonrpc import ServiceProxy
 access = ServiceProxy("http://127.0.0.1:8332")
 pwd = raw_input("Enter old wallet passphrase: ")
 pwd2 = raw_input("Enter new wallet passphrase: ")
-access.walletpassphrasechange(pwd, pwd2)
+if pwd2 == "":
+  print("Your about to remove the password of your wallet are you sure about this?")
+  while True:
+    awnser = raw_input("Y or N: ")
+    if awnser == "Y":
+      access.walletpassphrasechange(pwd, pwd2)
+      break
+    elif awnser == "N":
+      print("Stoped changes")
+      break
+    else:
+      print("Please awnser only with Y or N")
+else:
+  access.walletpassphrasechange(pwd, pwd2)
+      
