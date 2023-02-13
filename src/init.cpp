29f95a7de18b338b339632fded20fd2afaf2aa92
Shawn M <shawnvash@gmail.com>
2023-02-13 02:57:27 -0800

    Update init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -1,5 +1,5 @@
-// Copyright (c) 2009-2022 Satoshi Nakamoto
-// Copyright (c) 2009-2022 The Bitcoin developers
+// Copyright (c) 2009-2023 Satoshi Nakamoto
+// Copyright (c) 2009-2023 The Bitcoin developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
