6b4dcf35fefb6a830e486744fd7d88478862c2b5
NoNameDude <62181945+NoNameDude@users.noreply.github.com>
2023-02-14 11:07:00 +0100

    Updated Syntax
    raw_input = input

--- contrib/wallettools/walletchangepass.py
+++ contrib/wallettools/walletchangepass.py
@@ -1,11 +1,11 @@
 from jsonrpc import ServiceProxy
 access = ServiceProxy("http://127.0.0.1:8332")
-pwd = raw_input("Enter old wallet passphrase: ")
-pwd2 = raw_input("Enter new wallet passphrase: ")
+pwd = input("Enter old wallet passphrase: ")
+pwd2 = input("Enter new wallet passphrase: ")
 if pwd2 == "":
   print("Your about to remove the password of your wallet are you sure about this?")
   while True:
-    awnser = raw_input("Y or N: ")
+    awnser = input("Y or N: ")
     if awnser == "Y":
       access.walletpassphrasechange(pwd, pwd2)
       break
