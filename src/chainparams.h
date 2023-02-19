60aeae25672d39824c04ce6b23d5e3e99cd94ff3
Shawn M <shawnvash@gmail.com>
2023-02-18 20:52:47 -0800

    Update chainparams.h

--- src/chainparams.h
+++ src/chainparams.h
@@ -54,15 +54,13 @@ public:
         MAX_BASE58_TYPES
     };
     
-    const CMessageHeader::MessageStartChars& MessageStart() const { return pchMessageStart; }
-     int GetDefaultPort() const { return nDefaultPort; }
-    
-    
+    const MessageStartChars& MessageStart() const { return pchMessageStart; }
     bool MiningRequiresPeers() const { return fMiningRequiresPeers; }
     /** Default value for -checkmempool and -checkblockindex argument */
     bool DefaultConsistencyChecks() const { return fDefaultConsistencyChecks; }
     /** Policy: Filter transactions that do not match well-defined patterns */
     bool RequireStandard() const { return fRequireStandard; }
+    int GetDefaultPort() const { return nDefaultPort; }
     uint64_t PruneAfterHeight() const { return nPruneAfterHeight; }
     /** Make miner stop after a block is found. In RPC, don't return until nGenProcLimit blocks are generated */
     bool MineBlocksOnDemand() const { return fMineBlocksOnDemand; }
@@ -101,7 +99,6 @@ protected:
     int nMinReorganizationPeers;
 
     int nSwitchKimotoGravity;
-
 };
 
 /**
