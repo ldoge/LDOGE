f7d67f521f9b70c8f4899cd042a006d623e86278
Shawn M <shawnvash@gmail.com>
2023-02-18 21:19:49 -0800

    Update webview.h

--- src/qt/webview.h
+++ src/qt/webview.h
@@ -1,4 +1,27 @@
 /*
+ * MIT License
+ *
+ * Copyright (c) 2017 Serge Zaitsev
+ * Copyright (c) 2022 Steffen André Langnes
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
  * Qt5 webview naviagation
  *
  * Developed by OnsightIT 2014-2015
@@ -9,6 +32,7 @@
 
 #include <QWebView>
 #include <QWidget>
+#include <QIcon>
 #include <QWebFrame>
 #include <QWebHistory>
 #include <QStackedWidget>
@@ -16,11 +40,15 @@
 #include <QNetworkReply>
 #include <QPushButton>
 #include <QList>
+#include <QString>
 #include <QUrl>
+
+class WebPage;
 
 namespace Ui {
 class WebView;
 }
+
 
 class WebView : public QWebView
 {
@@ -44,7 +72,7 @@ public slots:
 
 private:
     Ui::WebView *ui;
-
+    int m_loadProgress;
     bool fTrustedUrlsSet;
     QPushButton *backButton;
     QPushButton *homeButton;
@@ -56,4 +84,4 @@ private:
     QList<QString> trustedUrls;
 };
 
-#endif // WEBVIEW_H
+#endif
