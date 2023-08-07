92d299415deefb55fdcd08f9e8ac9ab5d7a0a316
Shawn M <shawnvash@gmail.com>
2023-08-07 10:39:38 -0700

    Update bitcoin_id_ID.ts

--- src/qt/locale/bitcoin_id_ID.ts
+++ src/qt/locale/bitcoin_id_ID.ts
@@ -13,12 +13,12 @@
     </message>
     <message>
         <location line="+41"/>
-        <source>Copyright © 2009-2020 The Bitcoin developers
-Copyright © 2012-2020 The LiteDogecoin developers
-Copyright © 2014-2020 The BlackCoin developers</source>
-        <translation>Copyright © 2009-2020 para pengembang Bitcoin
-Copyright © 2012-2020 para pengembang LiteDogecoin
-Copyright © 2014 para pengembang LiteDoge</translation>
+        <source>Copyright © 2009-2024 The Bitcoin developers
+    Copyright © 2014-2024 The BlackCoin developers
+    Copyright © 2015-2024 The LiteDoge developers</source>
+        <translation>Copyright © 2009-2024 para pengembang Bitcoin
+Copyright © 2014-2024 para pengembang Blackcoin
+Copyright © 2015-2024 para pengembang LiteDoge</translation>
     </message>
     <message>
         <location line="+15"/>
