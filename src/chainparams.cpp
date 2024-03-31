23bfcb2d49e3063ce79a5f7aa55b7ca0513a65c3
Shawn M <shawnvash@gmail.com>
2024-03-30 21:16:59 -0700

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -54,7 +54,7 @@ static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesi
  *     CTxOut(empty)
  *   vMerkleTree: 12630d16a9
  */
-static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
+static CBlock CreateGenesisBlock(uint32_t nTime, uint32_t nNonce, uint nBits, int nVersion, const CAmount& genesisReward)
 {
     const char* pszTimestamp = "plz time stamp. stahp";
     const CScript genesisOutputScript = CScript() << ParseHex("0x0") << OP_CHECKSIG;
@@ -192,7 +192,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        convertSeed6(vFixedSeeds, pnSeed6_main, ARRAYLEN(pnSeed6_main));
+        convertSeed6(vFixedSeeds, pnSeed6, ARRAYLEN(pnSeed6));
     }
 
     virtual const CBlock& GenesisBlock() const { return genesis; }
@@ -251,8 +251,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = boost::assign::list_of(0x04)(0x88)(0xB2)(0x1E).convert_to_container<std::vector<unsigned char> >();
         base58Prefixes[EXT_SECRET_KEY] = boost::assign::list_of(0x04)(0x88)(0xAD)(0xE4).convert_to_container<std::vector<unsigned char> >();
 
-        convertSeed6(vFixedSeeds, pnSeed6_test, ARRAYLEN(pnSeed6_test));
-
+        
     }
     virtual Network NetworkID() const { return CChainParams::TESTNET; }
 };
