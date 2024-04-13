a79ed4de0eef74e30a057ce4d081f662e37a6685
Shawn M <shawnvash@gmail.com>
2024-04-13 02:51:18 -0700

    Update util.h

--- src/util.h
+++ src/util.h
@@ -3,6 +3,7 @@
 // Copyright (c) 2009-2024 The Litedoge developers
 // Distributed under the MIT/X11 software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef BITCOIN_UTIL_H
 #define BITCOIN_UTIL_H
 
